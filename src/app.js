465bfa0661ad7338beff808850338490bea85234
Patrick Boynton <dakilangmanok@gmail.com>
2019-07-25 15:10:21 -0400

    Added an event listener for adding posts.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import { ui } from './ui.js';
 
 document.addEventListener('DOMContentLoaded', getPosts);
 
+document.querySelector('.post-submit').addEventListener('click', addPost);
+
 function getPosts() {
     http.get('http://localhost:3000/posts')
         .then(data => ui.showPosts(data))
