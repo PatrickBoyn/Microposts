5a68f76108443cfe94f107ef1bc618e5e2602920
Patrick Boynton <dakilangmanok@gmail.com>
2019-07-25 00:30:27 -0400

    Added a then and catch.

--- src/app.js
+++ src/app.js
@@ -3,5 +3,5 @@ import { http } from './http.js';
 document.addEventListener('DOMContentLoaded', getPosts);
 
 function getPosts() {
-    http.get('http://localhost:3000/posts');
+    http.get('http://localhost:3000/posts').then(data => console.log(data)).catch(error => console.error(error))
 }
