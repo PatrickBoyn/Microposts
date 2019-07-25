51e16109bc273036735ad806f70c41bf49d88786
Patrick Boynton <dakilangmanok@gmail.com>
2019-07-25 15:16:54 -0400

    Added two variables and changed the function name.

--- src/app.js
+++ src/app.js
@@ -3,10 +3,16 @@ import { ui } from './ui.js';
 
 document.addEventListener('DOMContentLoaded', getPosts);
 
-document.querySelector('.post-submit').addEventListener('click', addPost);
+document.querySelector('.post-submit').addEventListener('click', submitPost);
 
 function getPosts() {
     http.get('http://localhost:3000/posts')
         .then(data => ui.showPosts(data))
         .catch(error => console.error(error))
+}
+
+function submitPost() {
+    const title = document.querySelector('#title').value;
+    const body = document.querySelector('#body').value; 
+    
 }
