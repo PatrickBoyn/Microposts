edfb428def991f1e64a3276e65290d89df952dcd
Patrick Boynton <dakilangmanok@gmail.com>
2019-07-25 13:44:18 -0400

    Imported the ui, and minor formatting.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import { http } from './http.js';
+import { ui } from './ui.js';
 
 document.addEventListener('DOMContentLoaded', getPosts);
 
 function getPosts() {
-    http.get('http://localhost:3000/posts').then(data => console.log(data)).catch(error => console.error(error))
+    http.get('http://localhost:3000/posts')
+        .then(data => console.log(data))
+        .catch(error => console.error(error))
 }
